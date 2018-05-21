a37462d370a546ced94f5fe7b454afdd1627a712
lasalesi <larsericmueller@gmail.com>
2018-05-21 20:47:49 +0200

    defined person and organisation

--- lifefair/lifefair/doctype/person/person.js
+++ lifefair/lifefair/doctype/person/person.js
@@ -4,5 +4,12 @@
 frappe.ui.form.on('Person', {
 	refresh: function(frm) {
 
+	},
+	validate: function(frm) {
+		cur_frm.set_value('full_name', frm.doc.first_name + " " + frm.doc.last_name);
+	},
+	/* update first characters when changing the name */
+	last_name: function(frm) {
+		cur_frm.set_value('first_characters', frm.doc.last_name.substring(0, 4).toUpperCase());
 	}
 });
