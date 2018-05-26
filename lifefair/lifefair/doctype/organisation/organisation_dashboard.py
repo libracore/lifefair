18d4a1c71d4fb9bea3a90d1f0872c46cb4d7fc50
lasalesi <larsericmueller@gmail.com>
2018-05-26 19:51:13 +0200

    moved address into separate doctype

---
+++ lifefair/lifefair/doctype/organisation/organisation_dashboard.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+   return {
+      'fieldname': 'organisation',
+      'transactions': [
+         {
+            'label': _('Address'),
+            'items': ['Address']
+         }
+      ]
+   }
