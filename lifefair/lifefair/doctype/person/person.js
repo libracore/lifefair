ad13a72a18f13995e5bdba1e47aadb6c06127b88
lasalesi <larsericmueller@gmail.com>
2018-05-30 22:12:01 +0200

    primary organisation function

--- lifefair/lifefair/doctype/person/person.js
+++ lifefair/lifefair/doctype/person/person.js
@@ -6,10 +6,22 @@ frappe.ui.form.on('Person', {
 
 	},
 	validate: function(frm) {
+		// set full name
 		cur_frm.set_value('full_name', frm.doc.first_name + " " + frm.doc.last_name);
+		// set primary company
+		find_primary_company(frm);
 	},
 	/* update first characters when changing the name */
 	last_name: function(frm) {
 		cur_frm.set_value('first_characters', frm.doc.last_name.substring(0, 4).toUpperCase());
 	}
 });
+
+function find_primary_company(frm) {
+	for (i = 0; i < frm.doc.organisations.length; i++) {
+		if (frm.doc.organisations[i].is_primary) {
+			cur_frm.set_value('primary_organisation', frm.doc.organisations[i].organisation);
+			break;
+		} 
+	} 
+}
