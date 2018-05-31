626dae55f285b46cfd27edd54d66a9e66032c8dc
lasalesi <larsericmueller@gmail.com>
2018-05-31 21:08:07 +0200

    address validation check (#753)

--- lifefair/lifefair/doctype/organisation_address/organisation_address.js
+++ lifefair/lifefair/doctype/organisation_address/organisation_address.js
@@ -4,5 +4,12 @@
 frappe.ui.form.on('Organisation Address', {
 	refresh: function(frm) {
 
+	},
+	validate: function(frm) {
+		// check if address is linked to an organisation or person
+		if ((!frm.doc.person) && (!frm.doc.organisation)) {
+			frappe.msgprint( __("Please select a link to an organisation or person."), __("Validation") );
+			frappe.validated=false;
+		}
 	}
 });
