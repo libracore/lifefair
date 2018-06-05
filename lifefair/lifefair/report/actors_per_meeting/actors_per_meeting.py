089799ec69076bbc5fc94fbc3d9ec720a9b2a2ce
lasalesi <larsericmueller@gmail.com>
2018-06-05 17:48:57 +0200

    bugfix export to Excel (#781)

--- lifefair/lifefair/report/actors_per_meeting/actors_per_meeting.py
+++ lifefair/lifefair/report/actors_per_meeting/actors_per_meeting.py
@@ -4,25 +4,34 @@
 from __future__ import unicode_literals
 import frappe
 
+
 def execute(filters=None):
     columns, data = [], []
     
-    columns = ["Meeting::100", "Block::200", "Person:Link/Person:100", "Name::200", "Role::200", "Show on Website"]
+    columns = ["Meeting::100", 
+               "Block::200", 
+               "Person:Link/Person:100", 
+               "Name::200", 
+               "Role::200", 
+               "Show on website",
+               "Description",
+               "Image"]
     if filters:
-        data = get_actors(meeting=filters.meeting)
+        data = get_actors(meeting=filters.meeting, as_list=True)
     else:
-        data = get_actors()
-            
+        data = get_actors(as_list=True)
+          
     return columns, data
 
-def get_actors(meeting=None):
+# use as_list=True in case of later Export to Excel
+def get_actors(meeting=None, as_list=True):
     sql_query = """SELECT 
                 `t1`.`title` AS `Meeting`,
                 `t2`.`title` AS `Block`,
                 `t3`.`person` AS `Person`,
                 `t3`.`person_name` AS `Name`,
                 `t3`.`role` AS `Role`,
-                `t4`.`show_on_website` AS `Show on Website`,
+                `t4`.`show_on_website` AS `Show on website`,
                 `t4`.`website_description` AS `Description`,
                 `t4`.`image` AS `Image`
             FROM `tabMeeting` AS `t1`
@@ -32,5 +41,8 @@ def get_actors(meeting=None):
     if meeting:
         sql_query += """ WHERE `t1`.`title` = '{0}'""".format(meeting)
     sql_query += """ ORDER BY `t1`.`title` ASC, `t2`.`title` ASC, `t3`.`idx` ASC"""
-    data = frappe.db.sql(sql_query, as_dict = True)
+    if as_list:
+        data = frappe.db.sql(sql_query, as_list = True)
+    else:
+        data = frappe.db.sql(sql_query, as_dict = True)
     return data
