06ae28614fe6aaaf9a9685626d46b69e1f7b5e18
lasalesi <larsericmueller@gmail.com>
2018-06-19 23:33:30 +0200

    api extension with block filter (#823)

--- lifefair/lifefair/doctype/person/person.py
+++ lifefair/lifefair/doctype/person/person.py
@@ -88,20 +88,37 @@ class Person(Document):
 #
 # call the API from
 #   /api/method/lifefair.lifefair.doctype.person.person.website_actors
+#   /api/method/lifefair.lifefair.doctype.person.person.website_actors?block=<block id>
 @frappe.whitelist(allow_guest=True)
-def website_actors():
-    sql_query = """SELECT 
-	 `t1`.`long_name`, 
+def website_actors(block=None):
+    if block:
+	sql_query = """SELECT 
+			 `t2`.`long_name`, 
+			 `t3`.`organisation`,
+			 `t3`.`function`,
+			 `t2`.`website_description`, 
+			 `t2`.`image`,
+			 `t2`.`first_characters`
+		FROM `tabBlock Actor` AS `t1`
+		LEFT JOIN `tabPerson` AS `t2` ON `t1`.`person` = `t2`.`name`
+		LEFT JOIN 
+		  (SELECT * FROM `tabPerson Organisation` WHERE `is_primary` = 1) AS `t3`
+		  ON `t2`.`name` = `t3`.`parent`
+		WHERE `t1`.`parent` = "{0}" AND `t2`.`show_on_website` = 1
+		ORDER BY `t2`.`first_characters` ASC;""".format(block)
+    else:
+	sql_query = """SELECT 
+	     `t1`.`long_name`, 
 	     `t2`.`organisation`,
-	 `t2`.`function`,
-	 `t1`.`website_description`, 
-	 `t1`.`image`,
-	 `t1`.`first_characters`
-       FROM (SELECT * FROM `tabPerson` WHERE `show_on_website` = 1) AS `t1` 
-       LEFT JOIN 
-	  (SELECT * FROM `tabPerson Organisation` WHERE `is_primary` = 1) AS `t2`
-	  ON `t1`.`name` = `t2`.`parent`
-       ORDER BY `t1`.`first_characters` ASC"""
+	     `t2`.`function`,
+	     `t1`.`website_description`, 
+	     `t1`.`image`,
+	     `t1`.`first_characters`
+	   FROM (SELECT * FROM `tabPerson` WHERE `show_on_website` = 1) AS `t1` 
+	   LEFT JOIN 
+	      (SELECT * FROM `tabPerson Organisation` WHERE `is_primary` = 1) AS `t2`
+	      ON `t1`.`name` = `t2`.`parent`
+	   ORDER BY `t1`.`first_characters` ASC"""
     people = frappe.db.sql(sql_query, as_dict=True)
 
     return people
