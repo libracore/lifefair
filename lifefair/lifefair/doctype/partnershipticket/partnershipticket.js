0c4b1501024ee397ea9d4f65fa3bb24c6f32174e
lasalesi <larsericmueller@gmail.com>
2018-07-16 14:50:55 +0200

    bugfix after rename, code re-imported

--- lifefair/lifefair/doctype/partnershipticket/partnershipticket.js
+++ lifefair/lifefair/doctype/partnershipticket/partnershipticket.js
@@ -3,6 +3,37 @@
 
 frappe.ui.form.on('Partnershipticket', {
 	refresh: function(frm) {
-
+		// add download vCard buton
+		frm.add_custom_button( __("Identify people"), function() {
+			identify_people(frm);
+		}).addClass("btn-primary");
+	},
+	validate: function(frm) {
+		secure_items(frm);
+		
+		apply_owner(frm);
 	}
 });
+
+function secure_items(frm) {
+	var tickets = cur_frm.doc.tickets;
+	var ticket_no = 1;
+	tickets.forEach(function(entry) {
+		frappe.model.set_value(entry.doctype, entry.name, "ticket_no", ticket_no);
+		ticket_no++;
+	});
+}
+
+function apply_owner(frm) {
+	frappe.call({
+		method: 'apply_owner',
+		doc: frm.doc
+    });
+}
+
+function identify_people(frm) {
+	frappe.call({
+		method: 'identify_people',
+		doc: frm.doc
+    });
+}
--- lifefair/lifefair/doctype/partnershipticket/partnershipticket.py
+++ lifefair/lifefair/doctype/partnershipticket/partnershipticket.py
@@ -7,4 +7,25 @@ import frappe
 from frappe.model.document import Document
 
 class Partnershipticket(Document):
-	pass
+    def apply_owner(self):
+        try:
+            owner = frappe.get_doc("User", self.responsible)
+        except:
+            frappe.msgprint( _("The selected responsible person is not yet a user. Please add the email as new user.") )
+        else:
+            self.owner = self.responsible
+            self.save()
+        return
+    
+    def identify_people(self):
+        for ticket in self.tickets:
+            if ticket.email:
+                person_matches = frappe.get_all('Person', filters={'email': ticket.email}, fields=['name'])
+                if person_matches:
+                    ticket.person = person_matches[0]['name']
+                    registration_matches = frappe.get_all('Registration', filters={'person': person_matches[0]['name'], 'meeting': self.meeting}, fields=['name'])
+                    if registration_matches:
+                        ticket.registration = registration_matches[0]['name']
+        self.save()
+
+    pass
