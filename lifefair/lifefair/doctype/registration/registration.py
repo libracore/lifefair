1e01f060f88d49cf6502fc43321e3702f7071a09
lasalesi <larsericmueller@gmail.com>
2018-08-02 16:42:37 +0200

    bigfix xing import: match fields

--- lifefair/lifefair/doctype/registration/registration.py
+++ lifefair/lifefair/doctype/registration/registration.py
@@ -18,58 +18,54 @@ def import_xing(content, meeting):
     new_pers = []
     
     # field definition
-    TICKETNO = 0                    # ticket number cell A
-    BARCODE = 1                     # barcode cell B
-    REMARKS = 2                     # remarks field ("Kategorie")
-    STATUS = 4                      # status ['Bezahlt', 'Storniert', 'Versendet']
-    DATE = 5                        # order date (13.07.2018, 16:56)
-    SALUTATION = 10
-    TITLE = 11
-    FIRST_NAME = 12
-    LAST_NAME = 13
-    EMAIL = 14
-    COMPANY = 15
-    PHONE = 25
-    STREET = 19
-    PINCODE = 20
-    CITY = 21
-    FUNCTION = 24
-    BLOCK = 26                      # contains IF.xx
-    TYPE = 3
-    PAYMENT = 23
-    INVOICENO = 22
-    
-    # regex block finder
-    p = re.compile('IF.\d\d')
-    
+    TICKETNO        = 0                     # ticket number cell A
+    BARCODE         = 1                     # barcode cell B
+    REMARKS         = 2                     # remarks field ("Kategorie")
+    STATUS          = 4                     # status ['Bezahlt', 'Storniert', 'Versendet']
+    DATE            = 5                     # order date (13.07.2018, 16:56)
+    SALUTATION      = 10
+    TITLE           = 11
+    FIRST_NAME      = 12
+    LAST_NAME       = 13
+    EMAIL           = 14
+    COMPANY         = 15
+    PHONE           = 50                    # AY
+    STREET          = 26                    # AA                    
+    PINCODE         = 28                    # AC
+    CITY            = 29                    # AD
+    FUNCTION        = 49                    # AX 
+    BLOCK           = 52                    # BA, contains IF.xx
+    TYPE            = 3
+    PAYMENT         = 44                    # AS
+    INVOICENO       = 43                    # AR
+       
     isfirst = True
     # read csv
-    elements = csv.reader(content.encode('utf-8').splitlines(), dialect='excel')
+    elements = unicode_csv_reader(content.splitlines())
     # process elements
     for element in elements:
         if isfirst:
             isfirst = False;
             continue
         # check if the ticket is already in the database
-        db_regs = frappe.get_all("Registration", filters={'ticket_number': element[TICKETNO].decode('utf-8')}, fields=['name'])
+        db_regs = frappe.get_all("Registration", filters={'ticket_number': element[TICKETNO]}, fields=['name'])
         if db_regs:
             # ticket is already in the database, update
             reg = frappe.get_doc("Registration", db_regs[0]['name'])
-            status = parse_status(element[STATUS].decode('utf-8'))
+            status = parse_status(element[STATUS])
             reg.status = status
-            reg.type = element[TYPE].decode('utf-8')
-            reg.payment = element[PAYMENT].decode('utf-8')
-            reg.invoice_number = element[INVOICENO].decode('utf-8')
-            reg.phone = element[PHONE].decode('utf-8')
+            reg.type = element[TYPE]
+            reg.payment = element[PAYMENT]
+            reg.invoice_number = element[INVOICENO]
+            reg.phone = element[PHONE]
             # find block
-            match_block = p.match(element[BLOCK].decode('utf-8'))
-            if match_block:
-                block = "{0} {1}".format(meeting, match_block)
-            else:
-                block = None
+            block = find_block(element[BLOCK], meeting)
             reg.block = block
-            reg.save()
-            frappe.db.commit()
+            try:
+                reg.save()
+                frappe.db.commit()
+            except Exception as e:
+                frappe.log_error("Import Xing Error", "Update Registration failed. {0}".format(e))                 
         else:
             # ticket is not in the database, create
             # check email address to find person
@@ -78,71 +74,88 @@ def import_xing(content, meeting):
                 person_name = db_person[0]['name']
             else:
                 # person not found, create new person
-                full_name = "{0} {1}".format(element[FIRST_NAME].decode('utf-8'), element[LAST_NAME].decode('utf-8'))
+                full_name = "{0} {1}".format(element[FIRST_NAME], element[LAST_NAME])
                 if element[TITLE]:
-                    long_name = "{0} {1} {2}".format(element[TITLE].decode('utf-8'), element[FIRST_NAME].decode('utf-8'), element[LAST_NAME].decode('utf-8'))
+                    long_name = "{0} {1} {2}".format(element[TITLE], element[FIRST_NAME], element[LAST_NAME])
                 else:
                     long_name = full_name
                 try:
-                    first_characters = element[LAST_NAME].decode('utf-8')[0:4].upper()
+                    first_characters = element[LAST_NAME][0:4].upper()
                 except:
                     try:
-                        first_characters = element[LAST_NAME].decode('utf-8').upper()
+                        first_characters = element[LAST_NAME].upper()
                     except:
                         first_characters = "NN"
                 person = frappe.get_doc({
                     'doctype': "Person",
-                    'first_name': element[FIRST_NAME].decode('utf-8'),
-                    'last_name': element[LAST_NAME].decode('utf-8'),
+                    'first_name': element[FIRST_NAME],
+                    'last_name': element[LAST_NAME],
                     'full_name': full_name,
                     'long_name': long_name,
                     'first_characters': first_characters,
-                    'email': element[EMAIL].decode('utf-8'),
-                    'company_phone': element[PHONE].decode('utf-8'),
-                    'title': element[TITLE].decode('utf-8'),
-                    'remarks': "From Xing, {1} @ {0}, {2}, {3} {4}".format(element[COMPANY].decode('utf-8'), element[FUNCTION].decode('utf-8'), 
-                        element[STREET].decode('utf-8'), element[PINCODE].decode('utf-8'), element[CITY].decode('utf-8'))
+                    'email': element[EMAIL],
+                    'company_phone': element[PHONE],
+                    'title': element[TITLE],
+                    'remarks': "From Xing, {1} @ {0}, {2}, {3} {4}".format(element[COMPANY], element[FUNCTION], 
+                        element[STREET], element[PINCODE], element[CITY])
                 })
-                person = person.insert()
-                person_name = person.name
-                frappe.db.commit()
-                new_pers.append(person_name)
+                try:
+                    person = person.insert()
+                    person_name = person.name
+                    frappe.db.commit()
+                    new_pers.append(person_name)
+                except Exception as e:
+                    frappe.log_error("Import Xing Error", "Insert Person failed. {0}".format(e))      
+                                  
             # create the new registration
             # find block
-            match_block = p.match(element[BLOCK].decode('utf-8'))
-            if match_block:
-                block = "{0} {1}".format(meeting, match_block)
-            else:
-                block = None
+            block = find_block(element[BLOCK], meeting)
             # parse date stamp (13.07.2018, 16:56)
-            date_fields = element[DATE].decode('utf-8').split(',')[0].split('.')
+            date_fields = element[DATE].split(',')[0].split('.')
             date = "{0}-{1}-{2}".format(date_fields[2], date_fields[1], date_fields[0])
             # parse status ['Bezahlt', 'Storniert', 'Versendet'] > [Tentative, Confirmed, Cancelled, Paid, Sent]
-            status = parse_status(element[STATUS].decode('utf-8'))
-            registration = frappe.get_doc({
-                'doctype': "Registration",
-                'person': person_name,
-                'meeting': meeting,
-                'block': block,
-                'date': date,
-                'remarks': element[REMARKS].decode('utf-8'),
-                'ticket_number': element[TICKETNO].decode('utf-8'),
-                'barcode': element[BARCODE].decode('utf-8'),
-                'type': element[TYPE].decode('utf-8'),
-                'payment': element[PAYMENT].decode('utf-8'),
-                'invoice_number': element[INVOICENO].decode('utf-8'),
-                'phone': element[PHONE].decode('utf-8'),
-                'status': status
-            })
-
-            registration = registration.insert()
-            reg_name = registration.name
-            frappe.db.commit()
-            new_regs.append(reg_name)
+            status = parse_status(element[STATUS])
+            try:
+                registration = frappe.get_doc({
+                    'doctype': "Registration",
+                    'person': person_name,
+                    'meeting': meeting,
+                    'block': block,
+                    'date': date,
+                    'remarks': element[REMARKS],
+                    'ticket_number': element[TICKETNO],
+                    'barcode': element[BARCODE],
+                    'type': element[TYPE],
+                    'payment': element[PAYMENT],
+                    'invoice_number': element[INVOICENO],
+                    'phone': element[PHONE],
+                    'status': status
+                })
+                registration = registration.insert()
+                reg_name = registration.name
+                frappe.db.commit()
+                new_regs.append(reg_name)
+            except Exception as e:
+                frappe.log_error("Import Xing Error", "Insert Registration failed. {0}".format(e))
 
         
     return { 'registrations': new_regs, 'people': new_pers }
 
+def find_block(block_field, meeting):
+    # regex block finder
+    p = re.compile('IF.\d\d')
+    # find block in the source text
+    match_block = p.match(block_field)
+    if match_block:
+        block = "{0} {1}".format(meeting, match_block.group(0))
+        # check if the block exists in the db
+        db_block = frappe.get_all("Block", filters={'name': block}, fields=['name'])
+        if not db_block:
+            block = None
+    else:
+        block = None  
+    return block
+        
 def parse_status(xing_status):
     status = None
     if xing_status == "Bezahlt":
@@ -152,3 +165,16 @@ def parse_status(xing_status):
     elif xing_status == "Versendet":
         status = "Sent"
     return status
+
+
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
