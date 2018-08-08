fffc4842ee59abf46622a49c09c28a34ca4edae2
lasalesi <larsericmueller@gmail.com>
2018-08-08 21:10:05 +0200

    new feature ticket overview (#935)

--- lifefair/lifefair/doctype/partnershipticket/partnershipticket.js
+++ lifefair/lifefair/doctype/partnershipticket/partnershipticket.js
@@ -7,6 +7,8 @@ frappe.ui.form.on('Partnershipticket', {
 		frm.add_custom_button( __("Identify people"), function() {
 			identify_people(frm);
 		}).addClass("btn-primary");
+		// load business card
+		display_overview(frm);
 	},
 	validate: function(frm) {
 		//secure_items(frm);
@@ -34,6 +36,17 @@ function apply_owner(frm) {
 function identify_people(frm) {
 	frappe.call({
 		method: 'identify_people',
-		doc: frm.doc
+		doc: frm.doc,
+		callback: function(response) {
+			display_overview(frm);
+		}
     });
+}
+
+function display_overview(frm) {
+    // render business card
+    var html = frappe.render_template('ticket_overview', frm.doc);
+    if (frm.fields_dict['ticket_overview_html']) {
+		$(frm.fields_dict['ticket_overview_html'].wrapper).html(html);
+    }	
 }
