79d58b41c6efe5e27f8f40edbb554655e5811bfd
lasalesi <larsericmueller@gmail.com>
2019-01-10 22:13:12 +0100

    Update person.py
    Hotfix: unlinked person displays a wrong address

--- lifefair/lifefair/doctype/person/person.py
+++ lifefair/lifefair/doctype/person/person.py
@@ -11,10 +11,10 @@ class Person(Document):
     def get_addresses(self):
         sql_query = ("""SELECT *  
 			FROM `tabOrganisation Address` 
-			WHERE `parent` = '{1}'
+			WHERE `person` = '{1}'
 			UNION SELECT *  
 			FROM `tabOrganisation Address`
-			WHERE `organisation` = '{0}' AND `for_shipping` = '1'
+			WHERE `organisation` = '{0}' AND `for_shipping` = '1' AND `organisation` = ""
             ORDER BY `is_private` DESC, `city` ASC""".format(self.primary_organisation, self.name))
         addresses = frappe.db.sql(sql_query, as_dict=True)
         return { 'addresses': addresses }
