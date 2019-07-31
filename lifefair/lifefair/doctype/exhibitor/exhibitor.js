4e5670ea84a21d94495dd91f26e5957ed7a9d3da
Chief5 <ben.helmy@gmail.com>
2019-07-31 15:04:43 +0200

    improved apiu calls

--- lifefair/lifefair/doctype/exhibitor/exhibitor.js
+++ lifefair/lifefair/doctype/exhibitor/exhibitor.js
@@ -4,5 +4,13 @@
 frappe.ui.form.on('Exhibitor', {
 	refresh: function(frm) {
 
-	}
+	},
+	validate: function(frm) {
+    // check length limit of description field
+    if (frm.doc.description.length > 1100) {
+        frappe.msgprint( __("Description too long. Please shorten below 1100 characters."), __("Validation") );
+        frappe.validated=false;
+    }
+}
+
 });
