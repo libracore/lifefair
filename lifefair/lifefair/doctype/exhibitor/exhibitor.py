d59d4e768d264fa470d3063b270591a1a426c59d
lasalesi <larsericmueller@gmail.com>
2019-08-14 21:01:23 +0200

    Update exhibitor.py
    Hotfix: testimonial 2 & 3 filter

--- lifefair/lifefair/doctype/exhibitor/exhibitor.py
+++ lifefair/lifefair/doctype/exhibitor/exhibitor.py
@@ -7,7 +7,7 @@ import frappe
 from frappe.model.document import Document
 
 class Exhibitor(Document):
-	pass
+    pass
 
 #
 # call the API from
@@ -78,40 +78,49 @@ def get_exhibitors(event=None):
                  `tP1`.`email` AS `email_1`,
                  `tP1`.`company_phone` AS `phone_1`,
                  IF(ISNULL(`tP1`.`image`), "https://sges.ch/wp-content/uploads/referenten-2019/avatar.png", `tP1`.`image`) AS `image_1`,
-             IF(ISNULL(`tTestimonial1`.`text`), "", CONCAT("«",`tTestimonial1`.`text`,"»")) AS `testimonial_1`,
+                 IF(ISNULL(`tTestimonial1`.`text`), "", CONCAT("«",`tTestimonial1`.`text`,"»")) AS `testimonial_1`,
                  `tP2`.`long_name` AS `person_2`,
                  `tP2`.`website_description` AS `website_description_2`,                
                  `tP2`.`email` AS `email_2`,
                  `tP2`.`company_phone` AS `phone_2`,
                  IF(ISNULL(`tP2`.`image`), "https://sges.ch/wp-content/uploads/referenten-2019/avatar.png", `tP2`.`image`) AS `image_2`,
-             IF(ISNULL(`tTestimonial2`.`text`), "", CONCAT("«",`tTestimonial2`.`text`,"»")) AS `testimonial_2`,  
+                 IF(ISNULL(`tTestimonial2`.`text`), "", CONCAT("«",`tTestimonial2`.`text`,"»")) AS `testimonial_2`,  
                  `tP3`.`long_name` AS `person_3`,
                  `tP3`.`website_description` AS `website_description_3`,
                  `tP3`.`email` AS `email_3`,
                  `tP3`.`company_phone` AS `phone_3`,                
                  IF(ISNULL(`tP3`.`image`), "https://sges.ch/wp-content/uploads/referenten-2019/avatar.png", `tP3`.`image`) AS `image_3`,
-             IF(ISNULL(`tTestimonial3`.`text`), "", CONCAT("«",`tTestimonial3`.`text`,"»")) AS `testimonial_3`
-                 
+                 IF(ISNULL(`tTestimonial3`.`text`), "", CONCAT("«",`tTestimonial3`.`text`,"»")) AS `testimonial_3`
              FROM `tabExhibitor`
              LEFT JOIN `tabExhibition Contact` AS `tPL1` ON `tabExhibitor`.`name` = `tPL1`.`parent` AND `tPL1`.`idx` = 1
              LEFT JOIN `tabPerson` AS `tP1` ON `tP1`.`name` = `tPL1`.`person`
              LEFT JOIN 
-		  (SELECT `parent`, `text`, `date` 
-		   FROM `tabPerson Quote` AS `t1`
-		   WHERE `t1`.`name` = (SELECT `name` 
-			       FROM `tabPerson Quote` AS `t2`
-				   WHERE `t2`.`parent` = `t1`.`parent`
-			       ORDER BY `date` DESC LIMIT 1)) AS `tTestimonial1`
-  ON `tTestimonial1`.`parent` = `tP1`.`name`
+               (SELECT `parent`, `text`, `date` 
+                FROM `tabPerson Quote` AS `t1`
+                WHERE `t1`.`name` = (SELECT `name` 
+                    FROM `tabPerson Quote` AS `t2`
+                    WHERE `t2`.`parent` = `t1`.`parent`
+                    ORDER BY `date` DESC LIMIT 1)) AS `tTestimonial1`
+               ON `tTestimonial1`.`parent` = `tP1`.`name`
              LEFT JOIN `tabExhibition Contact` AS `tPL2` ON `tabExhibitor`.`name` = `tPL2`.`parent` AND `tPL2`.`idx` = 2
              LEFT JOIN `tabPerson` AS `tP2` ON `tP2`.`name` = `tPL2`.`person`
              LEFT JOIN 
-  (SELECT `parent`, `text` FROM `tabPerson Quote` ORDER BY `date` DESC LIMIT 1) AS `tTestimonial2`
-  ON `tTestimonial2`.`parent` = `tP2`.`name`
+               (SELECT `parent`, `text`, `date` 
+                FROM `tabPerson Quote` AS `t11`
+                WHERE `t11`.`name` = (SELECT `name` 
+                    FROM `tabPerson Quote` AS `t22`
+                    WHERE `t22`.`parent` = `t11`.`parent`
+                    ORDER BY `date` DESC LIMIT 1)) AS `tTestimonial2`
+               ON `tTestimonial2`.`parent` = `tP2`.`name`
              LEFT JOIN `tabExhibition Contact` AS `tPL3` ON `tabExhibitor`.`name` = `tPL3`.`parent` AND `tPL3`.`idx` = 3
              LEFT JOIN `tabPerson` AS `tP3` ON `tP3`.`name` = `tPL3`.`person`
              LEFT JOIN 
-               (SELECT `parent`, `text` FROM `tabPerson Quote` ORDER BY `date` DESC LIMIT 1) AS `tTestimonial3`
+               (SELECT `parent`, `text`, `date` 
+                FROM `tabPerson Quote` AS `t111`
+                WHERE `t111`.`name` = (SELECT `name` 
+                    FROM `tabPerson Quote` AS `t222`
+                    WHERE `t222`.`parent` = `t111`.`parent`
+                    ORDER BY `date` DESC LIMIT 1)) AS `tTestimonial3`
                ON `tTestimonial3`.`parent` = `tP3`.`name`
              WHERE `meeting` = "{event}"
              ORDER BY `tabExhibitor`.`organisation` ASC;""".format(event=event)
