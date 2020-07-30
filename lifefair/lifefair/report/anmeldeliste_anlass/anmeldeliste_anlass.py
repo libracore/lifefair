835a3502cbf2a62bb9eacd34e252ff2c517e6602
lasalesi <larsericmueller@gmail.com>
2020-07-30 13:22:51 +0200

    hotfix: missing column values

--- lifefair/lifefair/report/anmeldeliste_anlass/anmeldeliste_anlass.py
+++ lifefair/lifefair/report/anmeldeliste_anlass/anmeldeliste_anlass.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2019, libracore and contributors
+# Copyright (c) 2019-2020, libracore and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
@@ -9,68 +9,67 @@ def execute(filters=None):
     columns, data = [], []
     
     columns = [
-                {"label": ("Kontakt KNT"), "fieldname": "Person", "fieldtype": "Link", "options": "Person", "width": 100},
-                {"label": ("Personenkürzel"), "fieldname": "item_group", "fieldtype": "Link", "options": "Item Group",  "width": 50},
-                {"label": ("Name"), "fieldname": "Namw", "fieldtype": "Data", "width": 200},
-                {"label": ("Geschlecht"), "fieldname": "Geschlecht", "fieldtype": "Data", "width": 100},
-                {"label": ("Zeile 1"), "fieldname": "Zeile 1", "fieldtype": "Data", "width": 100},
-                {"label": ("Zeile 2"), "fieldname": "Zeile 2", "fieldtype": "Data", "width": 100},
-                {"label": ("Geprüft von"), "fieldname": "Geprüft von", "fieldtype": "Data", "width": 100},
-                {"label": ("Registrierung"), "fieldname": "Registrierung", "fieldtype": "Link", "options": "Registration", "width": 100}, 
-                {"label": ("Gutscheincode"), "fieldname": "Gutscheincode", "fieldtype": "Data", "width": 200},
-                {"label": ("Anlass"), "fieldname": "Meeting", "fieldtype": "Data", "width": 200},
-                {"label": ("Teilnahme"), "fieldname": "Teilnahme", "fieldtype": "Data", "width": 200},
-                {"label": ("Block"), "fieldname": "Block", "fieldtype": "Link", "options": "Block", "width": 100}, 
-                {"label": ("Funktion %"), "fieldname": "Funktion", "fieldtype": "Data", "width": 150},
-                {"label": ("Organisation"), "fieldname": "Organisation", "fieldtype": "Link", "options": "Organisation", "width": 200}, 
-                {"label": ("Branche"), "fieldname": "Branche", "fieldtype": "Data", "width": 100},
-                {"label": ("Stakeholder"), "fieldname": "Stakeholder", "fieldtype": "Data", "width": 100},
-                {"label": ("Hierarchiestufe"), "fieldname": "Hierarchiestufe", "fieldtype": "Data", "width": 100},
-                {"label": ("Email"), "fieldname": "Email", "fieldtype": "Data", "width": 150},
-                {"label": ("Nur einmal kontaktieren"), "fieldname": "Nur einmal kontaktieren", "fieldtype": "Data", "width": 100},
-                {"label": ("Briefanrede"), "fieldname": "Briefanrede", "fieldtype": "Data", "width": 100},
-                {"label": ("Nachname"), "fieldname": "Last Name", "fieldtype": "Data", "width": 100}
+                {"label": ("Kontakt KNT"), "fieldname": "person", "fieldtype": "Link", "options": "Person", "width": 100},
+                {"label": ("Personenkürzel"), "fieldname": "chars", "fieldtype": "Data",  "width": 50},
+                {"label": ("Name"), "fieldname": "full_name", "fieldtype": "Data", "width": 200},
+                {"label": ("Geschlecht"), "fieldname": "geschlecht", "fieldtype": "Data", "width": 100},
+                {"label": ("Zeile 1"), "fieldname": "line1", "fieldtype": "Data", "width": 100},
+                {"label": ("Zeile 2"), "fieldname": "line2", "fieldtype": "Data", "width": 100},
+                {"label": ("Geprüft von"), "fieldname": "checked", "fieldtype": "Data", "width": 100},
+                {"label": ("Registrierung"), "fieldname": "registrierung", "fieldtype": "Link", "options": "Registration", "width": 100}, 
+                {"label": ("Gutscheincode"), "fieldname": "gutscheincode", "fieldtype": "Data", "width": 200},
+                {"label": ("Anlass"), "fieldname": "meeting", "fieldtype": "Data", "width": 200},
+                {"label": ("Teilnahme"), "fieldname": "teilnahme", "fieldtype": "Data", "width": 200},
+                {"label": ("Block"), "fieldname": "block", "fieldtype": "Link", "options": "Block", "width": 100}, 
+                {"label": ("Funktion %"), "fieldname": "funktion", "fieldtype": "Data", "width": 150},
+                {"label": ("Organisation"), "fieldname": "organisation", "fieldtype": "Link", "options": "Organisation", "width": 200}, 
+                {"label": ("Branche"), "fieldname": "branche", "fieldtype": "Data", "width": 100},
+                {"label": ("Stakeholder"), "fieldname": "stakeholder", "fieldtype": "Data", "width": 100},
+                {"label": ("Hierarchiestufe"), "fieldname": "hierarchiestufe", "fieldtype": "Data", "width": 100},
+                {"label": ("Email"), "fieldname": "email", "fieldtype": "Data", "width": 150},
+                {"label": ("Nur einmal kontaktieren"), "fieldname": "contact_only_once", "fieldtype": "Data", "width": 100},
+                {"label": ("Briefanrede"), "fieldname": "briefanrede", "fieldtype": "Data", "width": 100},
+                {"label": ("Nachname"), "fieldname": "last_name", "fieldtype": "Data", "width": 100}
             ]
-            
+
     if filters:
         data = get_data(meeting=filters.meeting, as_dict=True)
     else:
         data = get_data(as_dict=True)
-          
+
     return columns, data
 
-# use as_list=True in case of later Export to Excel
 def get_data(meeting="%", as_dict=True):
-    sql_query = """SELECT 
-         `tabRegistration`.`person` AS `Person`, 
-         `tabRegistration`.`participation` AS `Teilnahme`, 
-         `tabPerson`.`first_characters` AS `Personenkürzel`,
-         `tabPerson`.`long_name` AS `Name`,
-         `tabPerson`.`gender` AS `Geschlecht`,
-         `tabPerson`.`branche` AS `Branche`,
-         `tabPerson`.`stakeholder` AS `Stakeholder`,
-         `tabPerson`.`hierarchiestufe` AS `Hierarchiestufe`,
-         SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 1) AS `Zeile 1`,
-         IF (SUBSTRING_INDEX(SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 2), ';', -1) !=  
-             SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 1), 
-             SUBSTRING_INDEX(SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 2), ';', -1), "") AS `Zeile 2`,
-         (SELECT IF(`tabRegistration`.`is_checked` = 1, "Ja", "Nein")) AS `Geprüft von`,
-         `tabRegistration`.`name` AS `Registrierung`,
-         `tabRegistration`.`meeting` AS `Meeting`,
-         `tabPerson`.`primary_function` AS `Funktion`,
-         `tabPerson`.`primary_organisation` AS `Organisation`,
-         `tabRegistration`.`code` AS `Gutscheincode`,
-         `tabPerson`.`email` AS `Email`,
-         `tabPerson`.`nur_einmal_kontaktieren` AS `Nur einmal kontakieren`,
-         `tabRegistration`.`block` AS `Block`,
-         IF (`tabPerson`.`salutation` LIKE "%", 
+    sql_query = """SELECT
+         `tabRegistration`.`person` AS `person`,
+         `tabRegistration`.`participation` AS `teilnahme`,
+         `tabPerson`.`first_characters` AS `chars`,
+         `tabPerson`.`long_name` AS `full_name`,
+         `tabPerson`.`gender` AS `geschlecht`,
+         `tabPerson`.`branche` AS `branche`,
+         `tabPerson`.`stakeholder` AS `stakeholder`,
+         `tabPerson`.`hierarchiestufe` AS `hierarchiestufe`,
+         SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 1) AS `line1`,
+         IF (SUBSTRING_INDEX(SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 2), ';', -1) !=
+             SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 1),
+             SUBSTRING_INDEX(SUBSTRING_INDEX(`tabPerson`.`website_description`, ';', 2), ';', -1), "") AS `line2`,
+         (SELECT IF(`tabRegistration`.`is_checked` = 1, "Ja", "Nein")) AS `checked`,
+         `tabRegistration`.`name` AS `registrierung`,
+         `tabRegistration`.`meeting` AS `meeting`,
+         `tabPerson`.`primary_function` AS `funktion`,
+         `tabPerson`.`primary_organisation` AS `organisation`,
+         `tabRegistration`.`code` AS `gutscheincode`,
+         `tabPerson`.`email` AS `email`,
+         `tabPerson`.`nur_einmal_kontaktieren` AS `contact_only_once`,
+         `tabRegistration`.`block` AS `block`,
+         IF (`tabPerson`.`salutation` LIKE "%",
              CONCAT(`tabPerson`.`letter_salutation`, " ", `tabPerson`.`salutation`),
-             `tabPerson`.`letter_salutation`) AS `Briefanrede`,
-         `tabPerson`.`last_name` AS `Last Name`
+             `tabPerson`.`letter_salutation`) AS `briefanrede`,
+         `tabPerson`.`last_name` AS `last_name`
         FROM `tabRegistration`
         LEFT JOIN `tabPerson` ON `tabRegistration`.`person` = `tabPerson`.`name`
-        WHERE 
-          `meeting` LIKE '{0}' 
+        WHERE
+          `meeting` LIKE '{0}'
           AND `status` NOT IN ("Cancelled", "Abgemeldet")
         LIMIT 10000;""".format(meeting)
     if as_dict:
