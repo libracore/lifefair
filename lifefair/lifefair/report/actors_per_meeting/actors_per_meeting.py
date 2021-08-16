fe3090f7f99d4063ec0911e80721b86330bb4759
lasalesi <larsericmueller@gmail.com>
2021-08-16 09:16:59 +0200

    actors per meeting only appear once

--- lifefair/lifefair/report/actors_per_meeting/actors_per_meeting.py
+++ lifefair/lifefair/report/actors_per_meeting/actors_per_meeting.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, libracore (https://www.libracore.com) and contributors
+# Copyright (c) 2018-2021, libracore (https://www.libracore.com) and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
@@ -49,14 +49,14 @@ def get_actors(meeting=None, block=None, interests=None, as_list=True):
             LEFT JOIN `tabRegistration` AS `t5` ON (`t1`.`name` = `t5`.`meeting` AND `t4`.`name` = `t5`.`person`)
             LEFT JOIN `tabPerson Interest` AS `t6` ON (`t6`.`parent` = `t4`.`name`)
             LEFT JOIN `tabPerson Organisation` AS `t8` ON (`t8`.`parent` = `t4`.`name`)
-			LEFT JOIN  `tabOrganisation`  AS `t9` ON (`t9`.`name` = `t8`.`organisation`)"""
+            LEFT JOIN  `tabOrganisation`  AS `t9` ON (`t9`.`name` = `t8`.`organisation`)"""
     if meeting:
         sql_query += """ WHERE `t1`.`title` = '{0}'""".format(meeting)
     elif block:
         sql_query += """ WHERE `t2`.`title` = '{0}'""".format(block)
     elif interests:
         sql_query += """ WHERE `t2`.`interest_1` = '{0}' OR `t2`.`interest_2` = '{0}' OR `t2`.`interest_3` = '{0}'""".format(interests)		
-    sql_query += """ GROUP BY (CONCAT(`t2`.`title`, `t3`.`person`, `t3`.`person_role`))"""
+    sql_query += """ GROUP BY (CONCAT(`t2`.`title`, `t3`.`person`))"""          # removed per role filter (person multiple times) by request 2021-08-16
     sql_query += """ ORDER BY `t1`.`title` ASC, `t2`.`title` ASC, `t3`.`idx` ASC"""
     if as_list:
         data = frappe.db.sql(sql_query, as_list = True)
