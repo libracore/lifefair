51bcc8565a654d697a58e3f24218dedbcd1aef43
lasalesi <larsericmueller@gmail.com>
2022-07-19 13:01:16 +0200

    bugfix ticket overview

--- lifefair/lifefair/doctype/partnershipticket/partnershipticket.js
+++ lifefair/lifefair/doctype/partnershipticket/partnershipticket.js
@@ -1,51 +1,53 @@
-// Copyright (c) 2018, libracore and contributors
+// Copyright (c) 2018-2022, libracore and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Partnershipticket', {
-	refresh: function(frm) {
-		// add download vCard buton
-		frm.add_custom_button( __("Identify people"), function() {
-			identify_people(frm);
-		}).addClass("btn-primary");
-		// load business card
-		display_overview(frm);
-	},
-	validate: function(frm) {
-		//secure_items(frm);
-		
-		apply_owner(frm);
-	}
+    refresh: function(frm) {
+        // add download vCard buton
+        frm.add_custom_button( __("Identify people"), function() {
+            identify_people(frm);
+        }).addClass("btn-primary");
+        // load business card
+        if (frm.doc.tickets) {
+            display_overview(frm);
+        }
+    },
+    validate: function(frm) {
+        //secure_items(frm);
+        
+        apply_owner(frm);
+    }
 });
 
 frappe.ui.form.on('Partnership Ticket Item', {
-	tickets_add: function(frm, cdt, cdn) {
-		frappe.model.set_value(cdt, cdn, "name", Date.now().toString());
-	}
+    tickets_add: function(frm, cdt, cdn) {
+        frappe.model.set_value(cdt, cdn, "name", Date.now().toString());
+    }
 });
 
 function secure_items(frm) {
-	var tickets = cur_frm.doc.tickets;
-	var ticket_no = 1;
-	tickets.forEach(function(entry) {
-		frappe.model.set_value(entry.doctype, entry.name, "ticket_no", ticket_no);
-		ticket_no++;
-	});
+    var tickets = cur_frm.doc.tickets;
+    var ticket_no = 1;
+    tickets.forEach(function(entry) {
+        frappe.model.set_value(entry.doctype, entry.name, "ticket_no", ticket_no);
+        ticket_no++;
+    });
 }
 
 function apply_owner(frm) {
-	frappe.call({
-		method: 'apply_owner',
-		doc: frm.doc
+    frappe.call({
+        method: 'apply_owner',
+        doc: frm.doc
     });
 }
 
 function identify_people(frm) {
-	frappe.call({
-		method: 'identify_people',
-		doc: frm.doc,
-		callback: function(response) {
-			display_overview(frm);
-		}
+    frappe.call({
+        method: 'identify_people',
+        doc: frm.doc,
+        callback: function(response) {
+            display_overview(frm);
+        }
     });
 }
 
@@ -53,6 +55,6 @@ function display_overview(frm) {
     // render business card
     var html = frappe.render_template('ticket_overview', frm.doc);
     if (frm.fields_dict['ticket_overview_html']) {
-		$(frm.fields_dict['ticket_overview_html'].wrapper).html(html);
-    }	
+        $(frm.fields_dict['ticket_overview_html'].wrapper).html(html);
+    }    
 }
