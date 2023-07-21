73fb34e9b0a69bc57203fedad84bf61cf3eb34f2
TunaFregno <vjss05@gmail.com>
2023-07-21 10:15:24 +0200

    GTM Code auf der Website

--- lifefair/templates/pages/tickets.js
+++ lifefair/templates/pages/tickets.js
@@ -1822,12 +1822,16 @@ function loadEndMsg() {
 
 document.addEventListener("DOMContentLoaded", function(event) {
     // add change triggers here
+    // Google Tag Manager 
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-P8ZVTDK');
+	
     // process command line arguments
     get_arguments();
     
-    //~ var container = document.querySelector(".my-5");
-    //~ container.classList.remove("container");
-    //~ container.classList.add("container-fluid");
 });
 
 function checkWebSource(currentURL, previousURL) {
